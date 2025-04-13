61ba1e7fe2658e46999aa16fe021673302716b80
Markus <mako0005@stud.kea.dk>
2025-04-13 20:13:15 +0200

    animations

---
+++ js/front.js
@@ -0,0 +1,34 @@
+document.querySelector("#button1").addEventListener("click", openDialog);
+
+function openDialog() {
+  document.querySelector("#dialog1"). showModal();
+}
+
+document.querySelector("#closeBtn1"). addEventListener("click", closeDialog);
+function closeDialog() {
+    document.querySelector("#dialog1"). closel();
+  }
+
+  document.querySelector("#button2").addEventListener("click", openDialog2);
+
+function openDialog2() {
+  document.querySelector("#dialog2"). showModal();
+}
+
+document.querySelector("#closeBtn2"). addEventListener("click", closeDialog2);
+
+function closeDialog2() {
+    document.querySelector("#dialog2"). closel();
+  }
+
+  document.querySelector("#button3").addEventListener("click", openDialog3);
+
+function openDialog3() {
+  document.querySelector("#dialog3"). showModal();
+}
+
+document.querySelector("#closeBtn3"). addEventListener("click", closeDialog3);
+
+function closeDialog3() {
+    document.querySelector("#dialog3"). closel();
+  }
--- js/main.js
+++ js/main.js
@@ -1,21 +1,79 @@
 document.querySelector("#dollarsign").addEventListener("mouseover", dollarsignlight);
 document.querySelector("#dollarsign").addEventListener("mouseout", dollarsignUNlight);
 document.querySelector("#dollarsign").addEventListener("click", dollarsignInfo);
+
 document.querySelector("#coffee").addEventListener("mouseover", coffeelight);
 document.querySelector("#coffee").addEventListener("mouseout", coffeeUNlight);
 document.querySelector("#coffee").addEventListener("click", coffeeInfo);
+
 document.querySelector("#grafiti").addEventListener("mouseover", grafitilight);
 document.querySelector("#grafiti").addEventListener("mouseout", grafitiUNlight);
 document.querySelector("#grafiti").addEventListener("click", grafitiInfo);
 
 function dollarsignlight() {
-    document.querySelector("#dollarsign").style.color = "green";
-    console.log("dollarsignlight");
-    this.style.fill = #fff";
+  console.log("dollarsignlight");
+  document.querySelector("#dollarsign").style.fill = "#fff";
 }
 
 function dollarsignUNlight() {
-    document.querySelector("#dollarsign").style.color = "green;
-    console.log("dollarsignUNlight");
-    this.style.fill = "#000";
+  console.log("dollarsignUNlight");
+  document.querySelector("#dollarsign").style.fill = "#f00";
 }
+
+function dollarsignInfo() {
+  console.log("dollarsignInfo");
+
+  animateboxes();
+
+  document.querySelector("#efficiency").classList.remove("hide");
+  document.querySelector("#efficiency").classList.add("fadein");
+  document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#requirement").classList.add("fadein");
+}
+
+function coffeelight() {
+  console.log("coffeelight");
+  document.querySelector("#coffee").style.fill = "#fff";
+}
+
+function coffeeUNlight() {
+  console.log("coffeeUNlight");
+  document.querySelector("#coffee").style.fill = "#f00";
+}
+
+function coffeeInfo() {
+  console.log("coffeeInfo");
+
+  animateboxes();
+
+  document.querySelector("#efficiency").classList.remove("hide");
+  document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#efficiency").classList.add("fadein");
+  document.querySelector("#requirement").classList.add("fadein");
+}
+
+function grafitilight() {
+  console.log("grafitilight");
+  document.querySelector("#grafiti").style.fill = "#fff";
+}
+
+function grafitiUNlight() {
+  console.log("grafitiUNlight");
+  document.querySelector("#grafiti").style.fill = "#f00";
+}
+
+function grafitiInfo() {
+  console.log("grafitiInfo");
+
+  animateboxes();
+
+  document.querySelector("#efficiency").classList.remove("hide");
+  document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#efficiency").classList.add("fadein");
+  document.querySelector("#requirement").classList.add("fadein");
+}
+
+document.querySelector(".info-text > h2").textContent = "";
+document.querySelector(".info-text > article > p").textContent = "";
+document.querySelector("#efficiency").innerHTML = "";
+document.querySelector("#requirement").innerHTML = "";
